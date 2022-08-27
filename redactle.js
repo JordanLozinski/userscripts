daa1fb72eeef226f84d986f375689979a81def14
Jordan <jordanlozinski1@gmail.com>
2022-08-27 12:26:40 -0500

    Display the length of the word in the input box

--- redactle.js
+++ redactle.js
@@ -16,7 +16,7 @@ function r() {
         $(this).append(`<span class="tooltiptext">${len}</span>`)
     });
 
-    var style = $('<style id="tooltipstyle" type="text/css">.baffled{ background-color: #989898; position: relative; } .baffled .tooltiptext {visibility: hidden;background-color: #6289ff;color: #fff; font-style: normal;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;padding: 0 10px 0 10px;bottom: 50%;left: 50%;transform: translate(-50%, 0%);} .baffled:hover .tooltiptext {visibility: visible;} }</style>');
+    var style = $('<style id="tooltipstyle" type="text/css">.baffled{ background-color: rgba(0,0,0,0) !important; position: relative; } .baffled .tooltiptext {visibility: hidden;background-color: #6289ff;color: #fff; font-style: normal;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;padding: 0 10px 0 10px;bottom: 50%;left: 50%;transform: translate(-50%, 0%);} .baffled:hover .tooltiptext {visibility: visible;} .display-length{ font-size: 25px; padding: 0 10px;}</style>');
     $('html > head').append(style);
 
     // Slow solution that works
@@ -25,6 +25,21 @@ function r() {
     }).mouseleave(function() {
         $(this).children('.tooltiptext').css({"visibility": "hidden"});
     }); */
+
+    var input_row = $('.input-group');
+    var length_section = $('<div class="display-length"></div>');
+    input_row.append(length_section);
+
+    $(".input-group input").on('input', function (e) {
+        var val = $(this).val();
+        if (val.length > 0) {
+            length_section.html("length: " + val.length);
+        } else {
+            length_section.html("");
+        }
+        // Display the length of the word in the text input (trimmed) next to the text box in Redactle
+    });
+
 }
 
 setTimeout(r, 5000);
